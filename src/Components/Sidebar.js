bda21bdb174c8a86bdb12df2fe8ceed9ae09f5e0
Daniel Rodriguez <MissingRed@github.com>
2021-02-06 22:16:57 -0500

    Se agrega Sidebar responsive

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import "../Styles/Sidebar.css";
 // import app from "../Database/Base.js";
 import { AuthContext } from "../Database/Auth";
@@ -6,11 +6,90 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   const { currentUser } = useContext(AuthContext);
+  const [openSidebar, setOpenSidebar] = useState(false);
+  const sidebarMobile = useRef();
+
+  const handleClose = () => {
+    sidebarMobile.current.style.animation = "animation_nav_reverse 0.5s";
+    sidebarMobile.current.addEventListener("animationend", () =>
+      setOpenSidebar(false)
+    );
+  };
   return (
     <>
       <div className="sidebarS">
-        <div>
-          <h2>CODESHOP</h2>
+        <div className="navcontent">
+          <div className="contenedorBar">
+            <img
+              src="Img/menu.svg"
+              alt=""
+              onClick={() => setOpenSidebar(!openSidebar)}
+            />
+            {openSidebar ? (
+              <div className="sidebar-menu" ref={sidebarMobile}>
+                <div className="sidebarS1">
+                  <img src="Img/x.svg" alt="" onClick={handleClose} />
+                  <div className="space">
+                    <div className="navcontent1">
+                      <div className="options1">
+                        <ul>
+                          <li>
+                            <img src="Img/shopping-bag.svg" alt="" />
+                            <NavLink
+                              activeClassName="selected"
+                              className="link"
+                              to="/Store"
+                            >
+                              Store
+                            </NavLink>
+                          </li>
+                          <li>
+                            <img src="Img/bookmark.svg" alt="" />
+                            <NavLink
+                              activeClassName="selected"
+                              className="link"
+                              to="/Favorites"
+                            >
+                              Favorites
+                            </NavLink>
+                          </li>
+                          <li>
+                            <img src="Img/user.svg" alt="" />
+                            <NavLink
+                              activeClassName="selected"
+                              className="link"
+                              to="/Profile"
+                            >
+                              Profile
+                            </NavLink>
+                          </li>
+                          <li>
+                            <img src="Img/shopping-cart.svg" alt="" />
+                            <NavLink
+                              className="link"
+                              to="/Purchases"
+                              activeClassName="selected"
+                            >
+                              Purchases
+                            </NavLink>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="userItem1">
+                      <img src={currentUser.photoURL} alt="" />
+                      <p>{currentUser.displayName}</p>
+                      {/* <button onClick={() => app.auth().signOut()}>Cerrar Sesión</button> */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+            <h2>CODESHOP</h2>
+            <span />
+          </div>
           <div className="options">
             <ul>
               <li>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -62,7 +62,7 @@ const Home = () => {
             <div className="gridItems">
               <div className="game">
                 <div className="imgGame">
-                  <img src="Img/spider.jpg" alt="" />
+                  <img src="Img/spider.jpg" alt="" className="image" />
                 </div>
                 <p className="title">Spider-Man: Miles Morales</p>
                 <p className="price">US$ 61.49</p>
@@ -72,6 +72,7 @@ const Home = () => {
                   <img
                     src="https://cdn1.epicgames.com/400347196e674de89c23cc2a7f2121db/offer/AC%20KINGDOM%20PREORDER_STANDARD%20EDITION_EPIC_Key_Art_Portrait_640x854-640x854-288120c5573756cb988b6c1968cebd86.png?h=854&resize=1&w=640"
                     alt=""
+                    className="image"
                   />
                 </div>
                 <p className="title">Assassin's Creed Valhalla</p>
@@ -82,6 +83,7 @@ const Home = () => {
                   <img
                     src="https://cdn1.epicgames.com/b30b6d1b4dfd4dcc93b5490be5e094e5/offer/RDO_Standalone_EGS_PortraitProductImage_1200x1600_Deliverable-1200x1600-b59e0f9eaa6ef53e8e4e62515aabadc2.jpg?h=854&resize=1&w=640"
                     alt=""
+                    className="image"
                   />
                 </div>
                 <p className="title">Red Dead Redemption 2</p>
@@ -92,6 +94,7 @@ const Home = () => {
                   <img
                     src="https://cdn1.epicgames.com/ca93b6d41a4e41af864942d8f0a2a826/offer/uno-ult-store-portrait-1200x1600-1200x1600-068853fa8ff2-1200x1600-96a1e051d1b1ed8a9eb3c1625b08f624.jpg?h=854&resize=1&w=640"
                     alt=""
+                    className="image"
                   />
                 </div>
                 <p className="title">UNO® Ultimate Edition</p>
@@ -102,6 +105,7 @@ const Home = () => {
                   <img
                     src="https://cdn1.epicgames.com/cd14dcaa4f3443f19f7169a980559c62/offer/EGS_SidMeiersCivilizationVI_FiraxisGames_S2-860x1148-bffad83909595b7c5c60489a17056a59.jpg?h=854&resize=1&w=640"
                     alt=""
+                    className="image"
                   />
                 </div>
                 <p className="title">Civilization VI</p>
@@ -112,6 +116,7 @@ const Home = () => {
                   <img
                     src="https://cdn1.epicgames.com/catnip/offer/BL3TallNologo-1200x1440-c1200ae70b5e054025e296746d1987a8.jpg?h=854&resize=1&w=640"
                     alt=""
+                    className="image"
                   />
                 </div>
                 <p className="title">Borderlands 3</p>
@@ -122,6 +127,7 @@ const Home = () => {
                   <img
                     src="https://cdn1.epicgames.com/58dfcd1952ee48c1a1fa31c6ace5fe3d/offer/EGS_TheLongDark_HinterlandStudioInc_S6-1200x1600-3debfeb01e511b9e1221f973b6bb1fe7.jpg?h=854&resize=1&w=640"
                     alt=""
+                    className="image"
                   />
                 </div>
                 <p className="title">The Long Dark</p>
